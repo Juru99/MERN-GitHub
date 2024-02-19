eaba199e756bfef35d8c5ea975b8a060bac98d70
stegano9744 <stegano9744@gmail.com>
2024-02-19 20:57:18 +0900

    ✨ Feat #5. Added Sidebar and Logout
    Task list:
    - Create files in pages and components folder
    - Designed Sidebar Component
    - Designed Logout Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import ExplorePage from './pages/ExplorePage';
+import LikesPage from './pages/LikesPage';
+import Sidebar from './components/Sidebar';
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="flex text-white">
+      <Sidebar />
+      <div className="max-w-5xl my-5  mx-auto transition-all duration-300 flex-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/likes" element={<LikesPage />} />
+          {/* <Route path="/:username" element={<ProfilePage />} /> */}
+          {/* <Route path="*" element={<NotFoundPage />} /> */}
+        </Routes>
+        <footer>Footer</footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
