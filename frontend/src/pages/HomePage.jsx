140e3c86a00d828d0043dbce797a46a7710c5633
stegano9744 <stegano9744@gmail.com>
2024-02-22 23:45:23 +0900

    ✨ Feat #15: Added the ability to receive user information using the GitHub API
    Task list:
    - HomePage.jsx:  Get user profile and repos. Pass data to the ProfileInfo component and Repos component using props, respectively
    - ProfileInfo.jsx: Displays user information.
    - Repos.jsx: Shows a list of public repositories. Pass data to the repo component using props
    - Repo.jsx: Shows one public repository
    - App.jsx: Added Toaster from the react-hot-toast package
    - functions.js: changed the date format.
    - constants.js: Matched programming language icons
    - .eslintrc.cjs: turned off the prop-types check.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,16 +1,52 @@
+import { useCallback, useEffect, useState } from 'react';
 import ProfileInfo from '../components/ProfileInfo';
 import Repos from '../components/Repos';
 import Search from '../components/Search';
 import SortRepos from '../components/SortRepos';
+import toast from 'react-hot-toast';
+import Spinner from '../components/Spinner';
 
 const HomePage = () => {
+  const [userProfile, setUserProfile] = useState(null);
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const [sortType, setSortType] = useState('forks');
+
+  const getUserProfileAndRepos = useCallback(async () => {
+    setLoading(true);
+    try {
+      const userRes = await fetch('https://api.github.com/users/Juru99');
+      const userProfile = await userRes.json();
+      setUserProfile(userProfile);
+
+      const repoRes = await fetch(userProfile.repos_url);
+      const repos = await repoRes.json();
+      setRepos(repos);
+      console.log('userProfile:', userProfile);
+      console.log('repos:', repos);
+
+      setLoading(false);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    getUserProfileAndRepos();
+  }, [getUserProfileAndRepos]);
+
   return (
     <div className="m-4">
       <Search />
       <SortRepos />
       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
-        <ProfileInfo />
-        <Repos />
+        {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
+        {repos.length > 0 && !loading && <Repos repos={repos} />}
+
+        {loading && <Spinner />}
       </div>
     </div>
   );
