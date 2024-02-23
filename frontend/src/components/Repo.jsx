850885d3cc911b44de688b33e5bbea2bf4a4d7a9
stegano9744 <stegano9744@gmail.com>
2024-02-23 16:28:45 +0900

    ✨ Feat #16: Added 'Clone' URL copy clipboard.
    Task list:
    - Repo.jsx: When you click 'Clone', the 'handleCloneClick' function will display a success toast if the copy is successful, or an error toast if the copy fails.
    - Search.jsx: Removed 'sm:w-72' TailwindCSS property from 'form Tag'.

--- frontend/src/components/Repo.jsx
+++ frontend/src/components/Repo.jsx
@@ -2,9 +2,19 @@ import { FaCodeBranch, FaCopy, FaRegStar } from 'react-icons/fa';
 import { FaCodeFork } from 'react-icons/fa6';
 import { formatDate } from '../utils/functions';
 import { PROGRAMMING_LANGUAGES } from '../utils/constants';
+import toast from 'react-hot-toast';
 
 const Repo = ({ repo }) => {
   const formattedDate = formatDate(repo.created_at);
+
+  const handleCloneClick = async repo => {
+    try {
+      await navigator.clipboard.writeText(repo.clone_url);
+      toast.success('Repo URL cloned to clipboard!');
+    } catch (error) {
+      toast.error('Failed to clone repo URL to clipboard!');
+    }
+  };
   return (
     <li className="mb-10 ms-7">
       <span
@@ -35,6 +45,7 @@ const Repo = ({ repo }) => {
           <FaCodeFork /> {repo.forks_count}
         </span>
         <span
+          onClick={() => handleCloneClick(repo)}
           className="cursor-pointer bg-green-100 text-green-800 text-xs
 					font-medium px-2.5 py-0.5 rounded-full flex items-center gap-1"
         >
